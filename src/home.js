92c219b324827c72a7cf97b379d1ccc09a51bace
Alexandra <alexandra.lojj@gmail.com>
2026-09-05 04:10:24 +0300

    Menu Page Done

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ export function loadHomePage() {
     content.appendChild(div);
 
     div.innerHTML = `
-    <h1>Welcome to our restaurant!</h1>
+    <h1>Welcome to Axa Restaurant!</h1>
     <img src="https://healthnewshub.org/wp-content/uploads/2024/03/Fast-Food-Restaurants.jpg" width=200px>
     <p>Fresh food, cozy atmosphere, unforgettable moments.</p>
     `;
